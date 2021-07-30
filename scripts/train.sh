2a3469220fba0af4a70f14e19a08f32852f446a9
Song <quanpengsong@gmail.com>
2021-07-30 20:05:38 +0800

    :tada: update example shell script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=0 python main.py \
 --about "" \
---mode train --dataset CelebA --dataset_path ../celeba/images \
---use_tensorboard true \
---batch_size 8 --img_size 128 \
---exp_id "exp_main"
+--exp_id "exp-1" \
+--mode train \
+--start_iter 0 --end_iter 100000 \
+--preload_dataset true --cache_dataset true \
+--use_tensorboard true --save_loss true \
+--dataset CelebA \
+--batch_size 8 --img_size 256 \
+--train_path ./archive/celeba_hq/train \
+--test_path ./archive/celeba_hq/test \
+--eval_path ./archive/celeba_hq/train
