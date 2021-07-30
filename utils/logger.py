3e85500fc76e6ac7d965bdf4b6ad60034bbbf353
Song <quanpengsong@gmail.com>
2021-07-30 20:32:33 +0800

    :tada: update template

--- utils/logger.py
+++ utils/logger.py
@@ -1,15 +1,24 @@
 import tensorflow as tf
 
+is_tensorflow_v1 = tf.__version__.startswith('1.')
+
 
 class Logger(object):
     """Tensorboard logger."""
 
     def __init__(self, log_dir):
         """Initialize summary writer."""
-        self.writer = tf.summary.create_file_writer(log_dir)
+        if is_tensorflow_v1:
+            self.writer = tf.summary.FileWriter(log_dir)
+        else:
+            self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Add scalar summary."""
-        with self.writer.as_default():
-            tf.summary.scalar(tag, value, step=step)
-            self.writer.flush()
+        if is_tensorflow_v1:
+            summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+            self.writer.add_summary(summary, step)
+        else:
+            with self.writer.as_default():
+                tf.summary.scalar(tag, value, step=step)
+                self.writer.flush()
