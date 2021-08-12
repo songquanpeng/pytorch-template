bcbdf36068ef839e1839948c22d0cf108a55410d
Song <quanpengsong@gmail.com>
2021-08-12 21:48:26 +0800

    :tada: add a util function

--- utils/misc.py
+++ utils/misc.py
@@ -21,12 +21,16 @@ def get_datetime(short=False):
     return datetime.datetime.now().strftime(format_str)
 
 
-def str2bool(v):
-    return v.lower() in ['true']
-
-
 def get_commit_hash():
     process = subprocess.Popen(['git', 'log', '-n', '1'], stdout=subprocess.PIPE)
     output = process.communicate()[0]
     output = output.decode('utf-8')
     return output[7:13]
+
+
+def str2bool(v):
+    return v.lower() in ['true']
+
+
+def str2list(string, separator='-', target_type=int):
+    return list(map(target_type, string.split(separator)))
