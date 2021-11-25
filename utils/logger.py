ddc71f86c0e06eacd6d164cd06ba7051537f66b2
Song <quanpengsong@gmail.com>
2021-11-25 10:42:18 +0800

    :fire: remove tensorflow dependency

--- utils/logger.py
+++ utils/logger.py
@@ -1,6 +1,4 @@
-import tensorflow as tf
-
-is_tensorflow_v1 = tf.__version__.startswith('1.')
+from tensorboardX import SummaryWriter
 
 
 class Logger(object):
@@ -8,17 +6,8 @@ class Logger(object):
 
     def __init__(self, log_dir):
         """Initialize summary writer."""
-        if is_tensorflow_v1:
-            self.writer = tf.summary.FileWriter(log_dir)
-        else:
-            self.writer = tf.summary.create_file_writer(log_dir)
+        self.writer = SummaryWriter(log_dir)
 
     def scalar_summary(self, tag, value, step):
         """Add scalar summary."""
-        if is_tensorflow_v1:
-            summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-            self.writer.add_summary(summary, step)
-        else:
-            with self.writer.as_default():
-                tf.summary.scalar(tag, value, step=step)
-                self.writer.flush()
+        self.writer.add_scalar(tag, value, step)
